c66e4ff2b66a09f54d5d6de8c8889cc3b33de6e9
VijayBarve <vjaybarve@rediffmail.com>
2015-09-03 15:47:05 -0400

    V 0.0.12 fxn extract_flickrdb Sorts issue #2

--- R/extract_flickrdb.R
+++ R/extract_flickrdb.R
@@ -2,13 +2,23 @@
 #' @import sqldf
 #' @param dbname name of the database (Generated using function flickrtodatabase)
 #' @param outputfile output csv file
-#' @return dataframe containing the Flickr data from database
+#' @return dataframe containing Flickr data from database
 #' @examples \dontrun{
 #' mydat=extract_flickrdb("testdb","text.csv")
 #' }
 #' @export
 extract_flickrdb <- function(dbname=NA,outputfile=NA){
+  if(is.na(dbname)){
+    print("Please specify a database file name saved using bdsns")
+    return(NULL)
+  }
+  if (!file.exists(dbname)){
+    print(paste("Database file",dbname,"does not extst"))
+    return(NULL)
+  }
   dat <- sqldf("select * from Flickrrec", dbname = dbname)
-  write.csv(dat,outputfile)
+  if(!is.na(outputfile)){
+    write.csv(dat,outputfile)
+  }
   return(dat)
 }
